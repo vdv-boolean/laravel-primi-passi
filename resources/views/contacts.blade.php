2d4097ee07578fe593445a21133c8c32986839a5
vincenzodivita <divitavincenzo@icloud.com>
2023-06-28 10:13:01 +0200

    Add page

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Contatti</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <span>Questa è la pagina dei contatti</span>
+        <ul>
+            <li>
+                <a href="/">Home</a>  
+            </li>
+            <li>
+                <a href="/about-us">Chi siamo</a>  
+            </li>
+            <li>
+                <a href="/blog">Blog</a>  
+            </li>
+            <li>
+                <a href="/contacts">Contatti</a>  
+            </li>
+        </ul>
+    </body>
+</html>
